9ea1aa3fd4cef78695c1455ddb6cbdf7635f5497
Steve Karg <skarg@users.sourceforge.net>
2024-11-20 20:07:50 -0600

    Added local date and time to device object.

--- zephyr/subsys/bacnet_osif/bacnet_datetime.c
+++ zephyr/subsys/bacnet_osif/bacnet_datetime.c
@@ -17,6 +17,9 @@
 
 /* local time */
 static BACNET_DATE_TIME BACnet_Date_Time;
+static int16_t UTC_Offset_Minutes;
+/* note: need starting and stopping dates/times to determine DST*/
+static bool DST_Active;
 static struct mstimer Date_Timer;
 
 /**
@@ -24,20 +27,20 @@ static struct mstimer Date_Timer;
  */
 void datetime_sync(void)
 {
-    bacnet_time_t seconds, elapsed_seconds;
-    unsigned long milliseconds;
+	bacnet_time_t seconds, elapsed_seconds;
+	unsigned long milliseconds;
 
-    milliseconds = mstimer_elapsed(&Date_Timer);
-    elapsed_seconds = milliseconds/1000UL;
-    if (elapsed_seconds) {
-        mstimer_restart(&Date_Timer);
-        seconds = datetime_seconds_since_epoch(&BACnet_Date_Time);
-        seconds += elapsed_seconds;
-        datetime_since_epoch_seconds(&BACnet_Date_Time, seconds);
-        /* generate a hundredths value */
-        milliseconds -= (elapsed_seconds * 1000UL);
-        BACnet_Date_Time.time.hundredths = milliseconds / 10;
-    }
+	milliseconds = mstimer_elapsed(&Date_Timer);
+	elapsed_seconds = milliseconds / 1000UL;
+	if (elapsed_seconds) {
+		mstimer_restart(&Date_Timer);
+		seconds = datetime_seconds_since_epoch(&BACnet_Date_Time);
+		seconds += elapsed_seconds;
+		datetime_since_epoch_seconds(&BACnet_Date_Time, seconds);
+		/* generate a hundredths value */
+		milliseconds -= (elapsed_seconds * 1000UL);
+		BACnet_Date_Time.time.hundredths = milliseconds / 10;
+	}
 }
 
 /**
@@ -48,23 +51,24 @@ void datetime_sync(void)
  * @param dst_active [out] The DST flag
  * @return true if successful, false on error
  */
-bool datetime_local(
-    BACNET_DATE *bdate,
-    BACNET_TIME *btime,
-    int16_t *utc_offset_minutes,
-    bool *dst_active)
+bool datetime_local(BACNET_DATE *bdate, BACNET_TIME *btime,
+		    int16_t *utc_offset_minutes, bool *dst_active)
 {
-    datetime_sync();
-    if (bdate) {
-        datetime_copy_date(bdate, &BACnet_Date_Time.date);
-    }
-    if (btime) {
-        datetime_copy_time(btime, &BACnet_Date_Time.time);
-    }
-    (void)utc_offset_minutes;
-    (void)dst_active;
+	datetime_sync();
+	if (bdate) {
+		datetime_copy_date(bdate, &BACnet_Date_Time.date);
+	}
+	if (btime) {
+		datetime_copy_time(btime, &BACnet_Date_Time.time);
+	}
+	if (utc_offset_minutes) {
+		*utc_offset_minutes = UTC_Offset_Minutes;
+	}
+	if (dst_active) {
+		*dst_active = DST_Active;
+	}
 
-    return true;
+	return true;
 }
 
 /**
@@ -75,14 +79,23 @@ bool datetime_local(
  */
 void datetime_timesync(BACNET_DATE *bdate, BACNET_TIME *btime, bool utc)
 {
-    if (bdate) {
-        datetime_copy_date(&BACnet_Date_Time.date, bdate);
-    }
-    if (btime) {
-        datetime_copy_time(&BACnet_Date_Time.time, btime);
-    }
-    mstimer_restart(&Date_Timer);
-    (void)utc;
+	BACNET_DATE_TIME utc_time = { 0 };
+	int8_t dst_adjust_minutes = 0;
+
+	if (utc) {
+		datetime_copy_date(&utc_time.date, bdate);
+		datetime_copy_time(&utc_time.time, btime);
+		if (DST_Active) {
+			dst_adjust_minutes = 60;
+		}
+		datetime_utc_to_local(&BACnet_Date_Time, &utc_time,
+				      UTC_Offset_Minutes, dst_adjust_minutes);
+	} else {
+		datetime_copy_date(&BACnet_Date_Time.date, bdate);
+		datetime_copy_time(&BACnet_Date_Time.time, btime);
+	}
+	mstimer_restart(&Date_Timer);
+	(void)utc;
 }
 
 /**
@@ -90,5 +103,5 @@ void datetime_timesync(BACNET_DATE *bdate, BACNET_TIME *btime, bool utc)
  */
 void datetime_init(void)
 {
-    mstimer_set(&Date_Timer, 0);
+	mstimer_set(&Date_Timer, 0);
 }

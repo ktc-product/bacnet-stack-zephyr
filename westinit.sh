85193a631b815006d72b52f05eb6a6ab8de85abf
Steve Karg <skarg@users.sourceforge.net>
2025-10-01 14:48:03 -0500

    changed workspace to bacnet

--- westinit.sh
+++ westinit.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# projects are expecting a 'bacnet' folder here
-ln -s "$PWD" "${PWD%/*}/bacnet"
-cd -P .
 # setup our build environment
-echo $PWD
-ls -al
 west init -l .
 west update > /dev/null 2>&1
